fdb6fe2bbe607098215dcc536931e6c21e6f8993
oghenevwoganga <oghenevwoogaga@gmail.com>
2022-10-17 01:22:58 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point.
+ *
+ *Return: 0 (means no error)
+ **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
