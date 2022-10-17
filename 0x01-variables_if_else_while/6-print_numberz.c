1eed5cb1af8336e39ba15dd91009c0b0eece45a5
oghenevwoganga <oghenevwoogaga@gmail.com>
2022-10-17 01:42:09 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point.
+ *
+ *Return: 0 (means success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
